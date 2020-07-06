6ef48ed22041894a8dfe9a2039af34fa512fe5b9
Drew Page <drew@siegemedia.com>
2020-07-06 08:29:18 -0700

    feature: text-wrap div styling added

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -10,11 +10,11 @@
         </div>
         <div class="fix-12-12">
             <div class="fix-6-12">
+              <div class="text-wrap">
               <?php echo get_the_post_thumbnail( $post_id, array(500, 350)); ?>
               <h1><?php echo get_the_title(); ?></h1>
-              <!-- <div class="text-wrap"> -->
                 <p><?php echo the_content(); ?></p>
-              <!-- </div> -->
+              </div>
             </div>
         </div>
       </div>
