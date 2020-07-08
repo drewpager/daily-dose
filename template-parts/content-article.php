a20bc1cbc47f996af76abb941845deb8d7957f68
Drew Page <drew@siegemedia.com>
2020-07-08 05:43:52 -0700

    feature: removed some extra padding and thumbnail image to simplify

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -6,12 +6,12 @@
   <div class="content">
     <div class="container">
       <div class="wrap">
-        <div class="heading-div">
-        </div>
+        <!-- <div class="heading-div">
+        </div> -->
         <div class="fix-12-12">
             <div class="fix-6-12">
               <div class="text-wrap">
-              <?php echo get_the_post_thumbnail( $post_id, array(500, 350)); ?>
+              <!-- <?php echo get_the_post_thumbnail( $post_id, array(500, 350)); ?> -->
               <h1><?php echo get_the_title(); ?></h1>
                 <p><?php echo the_content(); ?></p>
               </div>
