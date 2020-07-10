ec3cf4faed62a060be0e2b0dd21110e9f7b7d99a
Drew Page <drew@siegemedia.com>
2020-07-10 07:08:24 -0700

    resolve: remove autoplay class from video popup div

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -12,8 +12,26 @@
             <div class="fix-6-12">
               <div class="text-wrap">
               <!-- <?php echo get_the_post_thumbnail( $post_id, array(500, 350)); ?> -->
-              <h1><?php echo get_the_title(); ?></h1>
-                <p><?php echo the_content(); ?></p>
+              <h1 style="text-align: center;"><?php echo get_the_title(); ?></h1>
+              <div class="videoThumbnail shadow rounded popupTrigger margin-bottom-3 ae-2" data-popup-id="<?php echo $id ?>">
+									<?php include(get_template_directory_uri() . "/assets/svg/video-icon-dark.svg")?>
+									<img src="<?php the_post_thumbnail(array(500, 300)); ?>" class="wide videoThumbnail" alt="Video Thumbnail"/>
+								</div>
+									<div class="popup" data-popup-id="<?php echo $id ?>">
+									<div class="close"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close"></use></svg></div>
+										<div class="content">
+											<div class="container">
+												<div class="wrap">
+													<div class="fix-10-12">
+														<div class="embedVideo popupContent">
+															<?php the_field('custom_youtube_embed'); ?>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+                <?php echo the_content(); ?></p>
               </div>
             </div>
         </div>
