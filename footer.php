ed76a5d787dfe93247675438d71bf77a890c0407
Drew Page <drew@siegemedia.com>
2020-07-11 06:57:00 -0700

    feature: updated new cleaner Facebook link

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
     </div>
     <div class="right">
       <ul class="menu crop">
-        <li><a href="https://www.facebook.com/Daily-Dose-103121998097124" rel="noreferrer" target="_blank"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#facebook"></use></svg></a></li>
+        <li><a href="https://www.facebook.com/thedailydosenow" rel="noreferrer" target="_blank"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#facebook"></use></svg></a></li>
         <li><a href="https://twitter.com/TheDailyDose18" rel="noreferrer" target="_blank"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#twitter"></use></svg></a></li>
         <!-- <li><a href="http://instagram.com/designmodo" target="_blank"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#instagram"></use></svg></a></li> -->
         <li><a href="https://www.youtube.com/channel/UCuYZAnKoZYXAKKpsi_-90Tg?sub_confirmation=1" target="_blank"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#youtube"></use></svg></a></li>
