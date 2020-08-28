8c7f985682bd2ec5a71e1d9016e529e0000b4d62
Drew Page <drew@siegemedia.com>
2020-08-28 05:58:00 -0700

    reverted back to scrollbar commit

--- front-page.php
+++ front-page.php
@@ -69,10 +69,21 @@
 								<li class="col-5-12 bottom" style="padding-bottom: 15px;">
 								<!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
 									<!-- Daily Dose Ads -->
-									<ins class="adsbygoogle"
+									<!-- <ins class="adsbygoogle"
 											style="display:inline-block;width:436px;height:300px;border-radius: 3px;"
 											data-ad-client="ca-pub-1041022037840931"
 											data-ad-slot="2103516715"></ins>
+									<script>
+											(adsbygoogle = window.adsbygoogle || []).push({});
+									</script> -->
+									<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+									<!-- Daily Dose Ads -->
+									<ins class="adsbygoogle"
+											style="display:block"
+											data-ad-client="ca-pub-1041022037840931"
+											data-ad-slot="2103516715"
+											data-ad-format="auto"
+											data-full-width-responsive="true"></ins>
 									<script>
 											(adsbygoogle = window.adsbygoogle || []).push({});
 									</script>
